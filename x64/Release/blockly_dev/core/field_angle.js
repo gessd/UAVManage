66d7820f61758e54c78ff4a9581234b06a7c1c15
gessd <346962839@qq.com>
2022-10-20 17:34:51 +0800

    blockly角度控件改为顺时针

--- x64/Release/blockly_dev/core/field_angle.js
+++ x64/Release/blockly_dev/core/field_angle.js
@@ -144,7 +144,7 @@ Blockly.FieldAngle.HALF = 100 / 2;
  * increase. Angle increases clockwise (true) or counterclockwise (false).
  * @const {boolean}
  */
-Blockly.FieldAngle.CLOCKWISE = false;
+Blockly.FieldAngle.CLOCKWISE = true;
 
 /**
  * The default offset of 0 degrees (and all angles). Always offsets in the
@@ -152,7 +152,7 @@ Blockly.FieldAngle.CLOCKWISE = false;
  * Usually either 0 (0 = right) or 90 (0 = up).
  * @const {number}
  */
-Blockly.FieldAngle.OFFSET = 0;
+Blockly.FieldAngle.OFFSET = 90;
 
 /**
  * The default maximum angle to allow before wrapping.
