9bfa6c3378bb9433f69c1a8cd784add582cbb26c
gessd <346962839@qq.com>
2026-01-19 10:51:52 +0800

    跟随升级版本

--- UAVManage/definesetting.h
+++ UAVManage/definesetting.h
@@ -2,8 +2,8 @@
 #include <QDir>
 //程序版本号
 #define _MajorNumber_ 4
-#define _MinorNumber_ 3
-#define _BuildNumber_ 3
+#define _MinorNumber_ 4
+#define _BuildNumber_ 0
 //#define _TestNumber_ "测试2"
 //新程序下载存放文件夹
 #define _NewVersionPath_	"/update"
